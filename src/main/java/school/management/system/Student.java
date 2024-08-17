59e2a7353a7783af78ce0bbea148504f5d430298
Highmaster <nzechideraann@gmail.com>
2024-08-17 14:18:39 +0100

    first commit

---
+++ src/main/java/school/management/system/Student.java
@@ -0,0 +1,53 @@
+package school.management.system;
+
+
+/*
+This class is responsible for keeping the track
+of students fees, name, id, grade and fees paid
+ */
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    /*
+    * To create a new student by initializing.
+    * Fees for every student is $30,000
+    * Fees paid initially is 0
+    * @param id id for the student: unique.
+    * @param name name of the student
+    * @param grade grade of the student
+    * */
+
+    public Student(int id, String name, int grade ) {
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+
+    }
+    /*
+    * used to update the student's grade
+    * @param grade new grade of the student
+    * */
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+    /*
+    * Keep adding the fees ti feesP  aid field
+    * Add the fees to the fees paid
+    * The school is going to receive the funds
+    *
+    * @param fees the fees that the student pays
+    * */
+
+    public void updateFeesPaid(int fees){
+        feesPaid += fees;
+    }
+
+
+}
